3497370bd3bf11c45c4982258aa995b9a6df201c
teblesz <mteblesz@gmail.com>
2023-03-26 01:25:57 +0100

    Add matchup cubit

---
+++ lib/matchup/cubit/matchup_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:data_repository/data_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:form_inputs/form_inputs.dart';
+import 'package:formz/formz.dart';
+
+part 'matchup_states.dart';
+
+class MatchupCubit extends Cubit<MatchupState> {
+  MatchupCubit(this._dataRepository) : super(const MatchupState());
+
+  final DataRepository _dataRepository;
+
+  void nickChanged(String value) {
+    final nick = Nick.dirty(value);
+    emit(
+      state.copyWith(
+        nick: nick,
+        status: Formz.validate([nick]),
+      ),
+    );
+  }
+
+  Future<void> logInWithCredentials() async {
+    if (!state.status.isValidated) return;
+    emit(state.copyWith(status: FormzStatus.submissionInProgress));
+    try {
+      //await _dataRepository.smth();
+      emit(state.copyWith(status: FormzStatus.submissionSuccess));
+    } catch (_) {
+      emit(state.copyWith(status: FormzStatus.submissionFailure));
+    }
+  }
+}
