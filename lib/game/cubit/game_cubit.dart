902f94f043df1275600855e6aab60c3e8c58ca55
teblesz <mteblesz@gmail.com>
2023-04-18 23:49:38 +0200

    Add draft gameLoop logic

--- lib/game/cubit/game_cubit.dart
+++ lib/game/cubit/game_cubit.dart
@@ -12,31 +12,28 @@ enum QuestStatus {
 }
 
 class GameCubit extends Cubit<GameState> {
-  GameCubit(this._dataRepository) : super(const GameState());
-
   final DataRepository _dataRepository;
 
-  // bool isQuestResultUnknown({required int questNumber}) {
-  //   if (state.questNumber < questNumber) return true;
-  //   if (state.questNumber == questNumber &&
-  //       state.status != GameStatus.questResults) return true;
-  //   return false;
-  // } // TODO use above thing in quest results preferably
+  // TODO this just seems like doing BLoC but around
+  GameCubit(this._dataRepository) : super(const GameState()) {
+    setupSquadSubscribtion(
+        _dataRepository.currentRoom.currentSquadId); // subs to first squad
+    _dataRepository.subscribeCurrentSquadIdWith(
+        doLogic: setupSquadSubscribtion); // subs to next squads
+  }
 
-  Stream<QuestStatus> streamQuestResult({required int questNumber}) {
-    //TODO trash this and rework
-    //TODO !!! exception for 4th
-    return _dataRepository
-        .streamMembersList(questNumber: questNumber)
-        .map((members) {
-      if (members.isEmpty) return QuestStatus.upcoming;
-      if (members.any((member) => member.secretVote == null)) {
-        return QuestStatus.ongoing;
-      }
-      return members.any((member) => member.secretVote == false)
-          ? QuestStatus.defeat
-          : QuestStatus.success;
-    });
+  @override
+  Future<void> close() {
+    _dataRepository.unsubscribeCurrentSquadId();
+    return super.close();
+  }
+
+  //todo draw tthis and check
+  void setupSquadSubscribtion(String currentSquadId) {
+    _dataRepository.unsubscribeSquadIsSubmitted();
+    _dataRepository.subscribeSquadWith(
+        squadId: _dataRepository.currentRoom.currentSquadId,
+        doLogic: doGameLoop);
   }
 
   Stream<List<Player>> streamPlayersList() {
@@ -72,12 +69,72 @@ class GameCubit extends Cubit<GameState> {
     );
   }
 
-// TODO !!! replace questNUmber with _datarepo.currentsquad.id
-  // TODO change state.questnumber!! subscribtion to mission field
-
   Future<void> submitSquad() async {
     await _dataRepository.submitSquad();
   }
+
+  /// steering the game course through states and squad props
+  Future<void> doGameLoop(Squad squad) async {
+    emit(state.copyWith(questNumber: squad.questNumber));
+    switch (state.status) {
+      case GameStatus.squadChoice:
+        if (squad.isSubmitted) {
+          emit(state.copyWith(status: GameStatus.squadVoting));
+        }
+        break;
+      case GameStatus.squadVoting:
+        if (squad.isApproved == null) return;
+        if (squad.isApproved!) {
+          emit(state.copyWith(status: GameStatus.questVoting));
+        } else {
+          if (_dataRepository.currentPlayer.isLeader) {
+            await _dataRepository.nextSquad(questNumber: squad.questNumber);
+            await _dataRepository.nextLeader();
+          }
+          emit(state.copyWith(status: GameStatus.squadChoice));
+        }
+        break;
+      case GameStatus.questVoting: //TODO
+        //leader listen for results and sets up isSuccessfull, //here exception for 4th quest
+        //everybody listens for is succ and then see results
+        // get resutls
+        // leader sets up is successfull flag or it's counted everytime
+        // popup results after circle progress idicator
+        break;
+      case GameStatus.questResults: //TODO
+        //giveinfoAboutVotes()
+        if (await _checkEndGameCondition()) {
+          emit(state.copyWith(status: GameStatus.gameResults));
+          return;
+        }
+        if (_dataRepository.currentPlayer.isLeader) {
+          await _dataRepository.nextLeader();
+        }
+        break;
+      case GameStatus.gameResults: //TODO
+        //add state here - voting to kill merlin
+        //present gaame results, give button to go back to lobby
+        // show players characters
+
+        break;
+    }
+  }
+
+  Future<bool> _checkEndGameCondition() async {
+    //TODO
+    // check quest votes and set up isSuccessfull
+    return true;
+  }
+
+  Future<void> closeQuestResults() async {
+    //TODO
+    if (_dataRepository.currentPlayer.isLeader) {
+      await _dataRepository.nextSquad(questNumber: state.questNumber + 1);
+    }
+    emit(state.copyWith(status: GameStatus.gameResults));
+  }
+
+  // TODO Add method using nextSquad
 
   //Subscribtion to isSubmitted
   // state = squadVOting
@@ -86,4 +143,32 @@ class GameCubit extends Cubit<GameState> {
   //         context,
   //         MaterialPageRoute(builder: (context) => const MissionPage()),
   //       );
+
+  //--------------
+
+  // bool isQuestResultUnknown({required int questNumber}) {
+  //   if (state.questNumber < questNumber) return true;
+  //   if (state.questNumber == questNumber &&
+  //       state.status != GameStatus.questResults) return true;
+  //   return false;
+  // } // TODO use above thing in quest results preferably
+  Stream<QuestStatus> streamQuestResult({required int questNumber}) {
+    //TODO trash this and rework
+    //TODO !!! exception for 4th
+    return _dataRepository
+        .streamMembersList(questNumber: questNumber)
+        .map((members) {
+      if (members.isEmpty) return QuestStatus.upcoming;
+      if (members.any((member) => member.secretVote == null)) {
+        return QuestStatus.ongoing;
+      }
+      return members.any((member) => member.secretVote == false)
+          ? QuestStatus.defeat
+          : QuestStatus.success;
+    });
+  }
+
+  void checkQuestInfo(int questNumber) {
+    //if quest.successfull ?? giveinfoAboutVotes()
+  }
 }
